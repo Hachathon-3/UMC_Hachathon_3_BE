ca1216a7c01c483b674afde7783c3cb84842efe1
DESKTOP-TROTBC9\samso <samsockbee@gmail.com>
2026-01-11 06:15:06 +0900

    feat: 서버 오류에 대한 에러 코드 추가

--- src/main/java/com/example/anxiety_senpai/global/apiPayload/code/GeneralErrorCode.java
+++ src/main/java/com/example/anxiety_senpai/global/apiPayload/code/GeneralErrorCode.java
@@ -8,10 +8,10 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum GeneralErrorCode implements BaseCode {
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "auth401", "인증이 필요합니다."),
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "card400", "요청 파라미터가 올바르지 않습니다.");
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "card400", "요청 파라미터가 올바르지 않습니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "server500", "서버 오류가 발생했습니다.");
 
     private final HttpStatus status;
     private final String code;
     private final String message;
 }
-
